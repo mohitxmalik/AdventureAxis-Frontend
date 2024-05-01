ce7c99af88a50bac3222bceed33fc4ec86cf4107
Mohit Malik <mohit1630.be20@chitkara.edu.in>
2024-05-01 13:45:52 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,1 @@
-// created a base url to call
-// export const BASE_URL = 'https://tours-travels-backend.onrender.com/api/v1'
-export const BASE_URL = "http://localhost:4000/api/v1";
+export const BASE_URL = "https://adventureaxis.onrender.com/api/v1";
